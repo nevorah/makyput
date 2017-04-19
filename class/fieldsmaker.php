5fa26fb759aa7258c364bfc77eae343bb2a2cc36
Tiago Neves <tiagoneves@tiago.neves@idealinvest.com.br>
2017-04-19 18:06:25 -0300

    comentarios do codigo em ingles

--- class/fieldsmaker.php
+++ class/fieldsmaker.php
@@ -2,15 +2,16 @@
 
 
 /**
- * funcao para construir campos
+ * POSTGRE - Build form fields
  * 
- * @param String $cnx
- * @param String $table
- * @param String $fields  
- * @param Boolean $tools 
+ * @param String $cnx connection of database
+ * @param String $table name of table
+ * @param String $fields how fields show in form
+ * @param Boolean $tools indicate that this form have a buttons
+ * @param String $savetype "direct","inList", "none"
  * 
 */
-function fieldsmaker_pg($cnx,$table,$fields="*",$tools=true) {
+function fieldsmaker_pg($cnx,$table,$fields="*", $savetype) {
     
     $result = pg_query($cnx, "SELECT $fields
                 FROM INFORMATION_SCHEMA.COLUMNS 
@@ -20,24 +21,25 @@ function fieldsmaker_pg($cnx,$table,$fields="*",$tools=true) {
     
     //print_r($rows);
     
+    //Variables
     $name ="";
     $type ="";
     $null ="";
     $size ="";
-    $frm = ""; //massa de dados finais
+    $frm = ""; //data of fields
     
-    //inicio da form e nome da tabela
+    //Start form to put fields and tools
     $frm = "<form>"
         ."<input type='hidden' name='table' value='$table'>";
     
-    for ($i=0;$i<count($rows);$i++) { //roda campos
+    for ($i=0;$i<count($rows);$i++) { //run looping to build fields
         
-        $name = $rows[$i]["column_name"]; //nome da coluna
-        $type = $rows[$i]["data_type"]; // tipo de dado
-        $null = $rows[$i]["is_nullable"]; // pode nulo
-        $size = $rows[$i]["character_maximum_length"]; // tamanho
+        $name = $rows[$i]["column_name"]; //get field name
+        $type = $rows[$i]["data_type"]; // get type name
+        $null = $rows[$i]["is_nullable"]; // if field is nullable
+        $size = $rows[$i]["character_maximum_length"]; // get size
         
-        switch ($type) { //inicio das decisoes para campos
+        switch ($type) { //switch to decide how type of tag ipunt and others little things
             case "char": 
                 $frm .= "$name: <input type='text' name='$name' maxlength=$size value=''><br>";
                 break;
@@ -61,10 +63,23 @@ function fieldsmaker_pg($cnx,$table,$fields="*",$tools=true) {
         
     }
     
-    $frm .= "</form>";
     
-    return $frm;
+    /* @todo create button
+     * button must be:
+     * if variable $savetype equal direct 
+     *      New
+     *      Clear
+     *      Save
+     * if variable $savetype equal inList
+     *      Add
+     *      Cancel
+     * 
+     */
+    
+    
+    $frm .= "</form>"; //close form tag
+    
+    return $frm; //return html
 }
-
 
 
